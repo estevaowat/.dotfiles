8510419ebb7651d559eb91259a67b27f4ebf275f
Estevão Watanabe <estevao.watanabe@gmail.com>
2021-03-09 01:41:30 -0300

    modify configs

--- setup/vs-code.sh
+++ setup/vs-code.sh
@@ -1,5 +1,4 @@
-# Install extensions
-code --install-extension azemoh.one-monokai
+# Install vscode extensions
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension EditorConfig.EditorConfig
 code --install-extension Equinusocio.vsc-material-theme
@@ -11,3 +10,4 @@ code --install-extension naumovs.color-highlight
 code --install-extension PKief.material-icon-theme
 code --install-extension ritwickdey.LiveServer
 code --install-extension shd101wyy.markdown-preview-enhanced
+code --install-extension antfu.icons-carbon
