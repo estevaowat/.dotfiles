9680940a55d72ffbbcf185b429fedd632e984081
Estevão Watanabe <estevao.watanabe@gmail.com>
2021-08-02 09:35:18 -0300

    configure vscode extensions

--- setup/vs-code.sh
+++ setup/vs-code.sh
@@ -1,13 +1,15 @@
 # Install vscode extensions
+code --install-extension antfu.icons-carbon
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension EditorConfig.EditorConfig
-code --install-extension Equinusocio.vsc-material-theme
-code --install-extension equinusocio.vsc-material-theme-icons
 code --install-extension esbenp.prettier-vscode
+code --install-extension igorming.useful-react-snippets
+code --install-extension jiza.theme-panda-high-contrast
 code --install-extension mikestead.dotenv
+code --install-extension ms-azuretools.vscode-docker
 code --install-extension ms-vscode-remote.remote-containers
 code --install-extension naumovs.color-highlight
 code --install-extension PKief.material-icon-theme
+code --install-extension Prisma.prisma
 code --install-extension ritwickdey.LiveServer
 code --install-extension shd101wyy.markdown-preview-enhanced
-code --install-extension antfu.icons-carbon
