4c21d0d99b4a0d676c4823200818aee9b1def091
Estevão Watanabe <estevao.watanabe@gmail.com>
2021-08-29 16:32:25 -0300

    configuring setup

--- setup/setup.sh
+++ setup/setup.sh
@@ -3,14 +3,14 @@ echo ----------- STARTING SETUP -------------
 echo ----------- Installing fonts -------------
 cp ~/.dotfiles/fonts/*.ttf /Library/Fonts/
 
-echo ----------- install homebrew ------------
+echo ----------- Installing files using HomeBrew ------------
 source ~/.dotfiles/setup/homebrew.sh
 
-# Create symbolic links to dotfiles
+echo ----------- Creating symbolic links -----------
 source ~/.dotfiles/install.sh
 
-echo ----------- setup oh-my-zsh ------------
+echo ----------- Installing oh-my-zsh ------------
 source ~/.dotfiles/setup/oh-my-zsh.sh
 
-echo ----------- setup Visual Studio Configuration ------------
+echo ----------- Configuring Visual Studio Code ------------
 source ~/.dotfiles/setup/vs-code.sh
