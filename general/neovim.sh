bdb7bd4710ddcfa44e9d52f9f65fb4e95a3cb529
Estevão Watanabe <estevao.watanabe@gmail.com>
2022-01-24 17:49:29 -0300

    Installing nvim plugins in terminal

--- general/neovim.sh
+++ general/neovim.sh
@@ -1,6 +1,11 @@
+#!/usr/bin/env sh
+# -*- coding: utf-8 -*-
 echo "Installing plug-vim"
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 echo "Creating symbolic link to init.vim"
-mkdir ~/.config/nvim
-ln -s $HOME/.dotfiles/nvim/.config/nvim/init.vim $HOME/.config/nvim/init.vim
+mkdir "$HOME"/.config/nvim
+ln -s "$HOME"/.dotfiles/nvim/.config/nvim/init.vim "$HOME"/.config/nvim/init.vim
+
+echo "Installing plugins for nvim"
+nvim --headless +PlugInstall +qall
