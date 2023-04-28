6161d33141a0e70841a1f906f590a07f311b909a
Estevão Watanabe <estevao.watanabe@gmail.com>
2023-04-28 00:24:28 -0300

    changing lsp config to double quote

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -1,19 +1,19 @@
-local lsp = require('lsp-zero')
+local lsp = require("lsp-zero")
 
-lsp.preset('recommended')
+lsp.preset("recommended")
 
-lsp.ensure_installed({
-    'eslint',
-    'tsserver',
-    'gopls',
-    'lua_ls'
+lsp.ensure_installed({ 
+    "eslint", 
+"tsserver",
+    "gopls",
+    "lua_ls"
 })
 
-lsp.configure('lua_ls', {
+lsp.configure("lua_ls", {
     settings = {
         Lua = {
             diagnostics = {
-                globals = { 'vim' }
+                globals = { "vim" }
             }
         }
     }
@@ -25,13 +25,13 @@ lsp.on_attach(function(_, bufnr)
         noremap = false
     }
 
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-    vim.keymap.set('n', 'rn', vim.lsp.buf.rename, opts)
-    vim.keymap.set('n', 'ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+    vim.keymap.set("n", "rn", vim.lsp.buf.rename, opts)
+    vim.keymap.set("n", "ca", vim.lsp.buf.code_action, opts)
 end)
 
 lsp.setup()
