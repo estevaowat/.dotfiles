ca524252217a91201585610e80c1b96c0b93a273
Estevão Watanabe <estevao.watanabe@gmail.com>
2024-04-27 02:01:49 -0300

    mapping for lsp gp

--- nvim/.config/nvim/lua/estevaowat/plugins/lsp.lua
+++ nvim/.config/nvim/lua/estevaowat/plugins/lsp.lua
@@ -12,6 +12,7 @@ return {
         "hrsh7th/cmp-cmdline",
         "L3MON4D3/LuaSnip",
         "saadparwaiz1/cmp_luasnip",
+        "rafamadriz/friendly-snippets",
         "j-hui/fidget.nvim"
     },
     config = function()
