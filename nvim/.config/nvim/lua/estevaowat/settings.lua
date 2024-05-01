36661e5c2ba86cbbbf4e6f346b8292018bcb5a6d
Estevão Watanabe <estevao.watanabe@gmail.com>
2024-05-01 14:58:59 -0300

    scrolloff 8

--- nvim/.config/nvim/lua/estevaowat/settings.lua
+++ nvim/.config/nvim/lua/estevaowat/settings.lua
@@ -3,6 +3,7 @@ vim.opt.relativenumber = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
+vim.opt.scrolloff = 8
 vim.opt.smartindent = false
 vim.opt.wrap = false
 vim.opt.hlsearch = true
