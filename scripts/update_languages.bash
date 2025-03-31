e61010b0ae0e6441d8987d948c6354ec9ee27c5c
Estevão Watanabe <estevao.watanabe@gmail.com>
2025-03-30 22:24:32 -0300

    feat: improving update java version

--- scripts/update_languages.bash
+++ scripts/update_languages.bash
@@ -1,7 +1,19 @@
 #! /usr/bin/env bash
 
 for tool in $(asdf plugin list); do
-    asdf install "$tool" latest
+    echo "updating $tool..."
+
+    if [[ "$tool" = 'java' ]]; then
+        lts_version=$(asdf list all java | rg temurin | rg -v jre | rg LTS | tail -1)
+        echo "java latest version $lts_version"
+        asdf install "$tool" $lts_version
+    else
+        asdf install "$tool" latest
+    fi
+
+    echo "$tool updated!"
+
+    echo "******************"
 done
 
 echo "set up all languages to latest version"
