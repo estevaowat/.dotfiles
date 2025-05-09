c5163fbfb9ea45520fabe23c9d8ac69a2705cb38
Estevão Watanabe <estevao.watanabe@gmail.com>
2025-05-09 00:14:34 -0300

    feat: configuring neovim

--- nvim/.config/nvim/lua/estevaowat/plugins/lsp.lua
+++ nvim/.config/nvim/lua/estevaowat/plugins/lsp.lua
@@ -65,20 +65,6 @@ return {
 					local lspconfig = require("lspconfig")
 					lspconfig.lua_ls.setup({
 						capabilities = capabilities,
-						settings = {
-							Lua = {
-								runtime = { version = "Lua 5.1" },
-								diagnostics = {
-									globals = {
-										"vim",
-										"it",
-										"describe",
-										"before_each",
-										"after_each",
-									},
-								},
-							},
-						},
 					})
 				end,
 				["gopls"] = function()
--- nvim/.config/nvim/lua/estevaowat/plugins/telescope.lua
+++ nvim/.config/nvim/lua/estevaowat/plugins/telescope.lua
@@ -1,15 +1,18 @@
 return {
- 'nvim-telescope/telescope.nvim', 
- tag = '0.1.6',
-      dependencies = { 'nvim-lua/plenary.nvim' },
-      config = function() 
-local builtin = require('telescope.builtin')
-vim.keymap.set('n','<leader>ff', builtin.find_files, {})
-vim.keymap.set('n','<leader>fg', builtin.git_files, {})
-vim.keymap.set('n','<leader>fr', builtin.live_grep, {})
-vim.keymap.set('n','<leader>fb', builtin.buffers, {})
-vim.keymap.set('n','<leader>fh', builtin.help_tags, {})
-vim.keymap.set('n','<leader>fs', function()
-    builtin.grep_string({search = vim.fn.input("Grep > ") })
-      end    )
-  end  }
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.6",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local builtin = require("telescope.builtin")
+
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "find files using telescope" })
+		vim.keymap.set("n", "<leader>fg", builtin.git_files, { desc = "find files in a git project using telescope" })
+		vim.keymap.set("n", "<leader>fk", builtin.keymaps, { silent = true })
+		vim.keymap.set("n", "<leader>fr", builtin.live_grep, { desc = "find content inside files using telescope" })
+		vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+		vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+		vim.keymap.set("n", "<leader>fs", function()
+			builtin.grep_string({ search = vim.fn.input("Grep > ") })
+		end)
+	end,
+}
